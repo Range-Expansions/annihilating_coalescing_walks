9ca126087d2396fce3e450aa0d8cb04e9abf2d5d
bryan <btweinstein@gmail.com>
2015-02-06 12:05:09 -0500

    First commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='annihilating_coalescing_walks',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Bryan Weinstein',
+    author_email='',
+    description=''
+)
