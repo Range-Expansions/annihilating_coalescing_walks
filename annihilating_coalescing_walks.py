51a78cb845008981323d309eb5b6ac59031a0454
bryan <btweinstein@gmail.com>
2015-02-06 19:00:30 -0500

    Things appear to be working.

--- annihilating_coalescing_walks.py
+++ annihilating_coalescing_walks.py
@@ -163,13 +163,15 @@ class Neutral_Lattice_Simulation():
         self.lattice = Lattice(lattice_size, num_types)
 
         self.time_array = None
-        self.lattice_array = None
+        self.lattice_history = None
         self.record_every = record_every
 
     def run(self, num_steps):
 
         self.time_array = np.zeros(num_steps + 1)
-        self.lattice_array = np.ones((num_steps, self.lattice_size))
+        self.lattice_history = -1*np.ones((num_steps, self.lattice_size))
+
+        self.lattice_history[0, :] = self.lattice.get_lattice_from_walls()
 
         cur_time = 0
         time_remainder = 0
@@ -214,7 +216,7 @@ class Neutral_Lattice_Simulation():
 
                 if time_remainder >= 1: # Record this step
                     self.time_array[num_recorded] = cur_time
-                    self.lattice_array[num_recorded] = self.lattice.get_lattice_from_walls()
+                    self.lattice_history[num_recorded, :] = self.lattice.get_lattice_from_walls()
 
                     num_recorded += 1
                     time_remainder -= 1
