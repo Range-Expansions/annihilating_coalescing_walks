22840ece0f0660a96ae6908a4de470bc83a873a9
btweinstein <btweinstein@gmail.com>
2015-02-08 12:26:19 -0500

    Enabling cython.

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
 from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import cython_gsl
+import numpy as np
+
+extensions = [
+    Extension("annihilating_coalescing_walks", sources=["annihilating_coalescing_walks.pyx"], language="c",
+              libraries = cython_gsl.get_libraries(),
+              library_dirs = [cython_gsl.get_library_dir()],
+              include_dirs = [cython_gsl.get_cython_include_dir(), np.get_include()])
+]
 
 setup(
     name='annihilating_coalescing_walks',
@@ -7,6 +18,8 @@ setup(
     url='',
     license='',
     author='Bryan Weinstein',
-    author_email='',
-    description=''
+    author_email='bweinstein@seas.harvard.edu',
+    description='',
+    include_dirs = [cython_gsl.get_include(), np.get_include()],
+    ext_modules = cythonize(extensions, annotate=True)
 )
