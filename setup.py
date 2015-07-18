2e6520e6042bfc427b517187016f2b4948c15158
btweinstein <btweinstein@gmail.com>
2015-07-18 17:27:43 -0400

    Renamed files so that the package names are shorter.

--- setup.py
+++ setup.py
@@ -5,11 +5,13 @@ import cython_gsl
 import numpy as np
 
 extensions = [
-    Extension("annihilating_coalescing_walks.annihilating_coalescing_walks", sources=["annihilating_coalescing_walks/annihilating_coalescing_walks.pyx"],
+    Extension("annihilating_coalescing_walks.annihilating_coalescing_walks",
+              sources=["annihilating_coalescing_walks/linear.pyx"],
               language="c++", libraries = cython_gsl.get_libraries(),
               library_dirs = [cython_gsl.get_library_dir()],
               include_dirs = [cython_gsl.get_cython_include_dir(), np.get_include()]),
-    Extension("annihilating_coalescing_walks.annihilating_coalescing_walks_inflation", sources=["annihilating_coalescing_walks/annihilating_coalescing_walks_inflation.pyx"],
+    Extension("annihilating_coalescing_walks.annihilating_coalescing_walks_inflation",
+              sources=["annihilating_coalescing_walks/inflation.pyx"],
               language="c++", libraries = cython_gsl.get_libraries(),
               library_dirs = [cython_gsl.get_library_dir()],
               include_dirs = [cython_gsl.get_cython_include_dir(), np.get_include()])
